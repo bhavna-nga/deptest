45a97a599923cb6989ac3210d758c8f7d57788bb
Lata <lata@newgenapps.com>
2018-12-18 15:31:12 +0530

    Updates

---
+++ precache-manifest.6eaee49bd719c0bf624cbf73e4bf46e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deptest/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5d9a5423453a3728f923",
+    "url": "/deptest/static/js/runtime~main.5d9a5423.js"
+  },
+  {
+    "revision": "fd26f01cb5c56188f0da",
+    "url": "/deptest/static/js/main.fd26f01c.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/deptest/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "fd26f01cb5c56188f0da",
+    "url": "/deptest/static/css/main.05bedd17.chunk.css"
+  },
+  {
+    "revision": "8d5da40faa4aef922d593d972af80684",
+    "url": "/deptest/index.html"
+  }
+];
